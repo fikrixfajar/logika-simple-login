00aa78e9a78bb360d13099d7e5273a676031036b
fikrixfajar <fikrifajar.18@gmail.com>
2022-08-02 09:17:39 +0700

    first commmit - Membuat tampilan sederhana navbar

---
+++ script.js
@@ -0,0 +1,25 @@
+let root = document.getElementById("root");
+let nav = document.getElementById("nav");
+
+
+
+
+
+
+
+
+
+
+
+
+// function navbar () {
+//     return (
+//         <div class="navbar">
+//             <input placeholder="username" id="usernameInput"></input>
+//             <input placeholder="password" id="passwordInput"></input>
+//             <button onClick="onLogin">Login</button>
+
+//         </div>
+//     )
+
+// }
